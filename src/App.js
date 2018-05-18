f9474576eb765ecfb1aab483529d675cda604e51
Max Bilbow <maxbilbow@gmail.com>
2018-05-19 00:56:30 +0100

    Added a thing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyFirstThing from "./components/MyFirstThing";
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          {new MyFirstThing().render()}
       </div>
     );
   }
---
+++ src/components/MyFirstThing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MyFirstThing.css';
+
+class MyFirstThing extends Component {
+    render() {
+
+        return (
+            <div className="MyFirstThing">
+                <p className="MyFirstThing-intro">
+                    Here is some stuff innit!
+                </p>
+            </div>
+        );
+    }
+}
+
+export default MyFirstThing;
